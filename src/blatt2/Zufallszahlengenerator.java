e389691d442937ca27742fc0ceffac9c9dfbd142
joeysigrist <joeysigrist@Joey-Laptop-Ubuntu>
2017-05-10 21:00:30 +0200

    pdf und besser

--- src/blatt2/Zufallszahlengenerator.java
+++ src/blatt2/Zufallszahlengenerator.java
@@ -1,69 +1,80 @@
 package blatt2;
 
 import java.util.Random;
+
 /**
  * 
- *  @author Alexander Neuwirth
- *  @author Leonhard Segger
- *  @author Jonathan Sigrist
+ * @author Alexander Neuwirth
+ * @author Leonhard Segger
+ * @author Jonathan Sigrist
  */
-public class Zufallszahlengenerator 
-{
-	private int current_val,mod,increment,mult;
+public class Zufallszahlengenerator {
+	private int current_val, mod, increment, mult;
 	private boolean java;
 	private Random rand;
-	
+
 	/**
 	 * Konstruktor
-	 * @param mult			Multiplikator
-	 * @param increment		Increment
-	 * @param mod			Modulo
-	 * @param seed			Startwert
+	 * 
+	 * @param mult
+	 *            Multiplikator
+	 * @param increment
+	 *            Increment
+	 * @param mod
+	 *            Modulo
+	 * @param seed
+	 *            Startwert
 	 */
-	public Zufallszahlengenerator(int mult, int increment, int mod,int seed)
-	{
-		this(mult,increment,mod,seed,false);
+	public Zufallszahlengenerator(int mult, int increment, int mod, int seed) {
+		this(mult, increment, mod, seed, false);
 	}
+
 	/**
 	 * Konstruktor
-	 * @param mult			Multiplikator
-	 * @param increment		Increment
-	 * @param mod			Modulo
-	 * @param seed			Startwert
-	 * @param use_java 		Soll Random.class verwendet werden
+	 * 
+	 * @param mult
+	 *            Multiplikator
+	 * @param increment
+	 *            Increment
+	 * @param mod
+	 *            Modulo
+	 * @param seed
+	 *            Startwert
+	 * @param use_java
+	 *            Soll Random.class verwendet werden
 	 */
-	public Zufallszahlengenerator(int mult, int increment, int mod,int seed,boolean use_java)
-	{
+	public Zufallszahlengenerator(int mult, int increment, int mod, int seed, boolean use_java) {
 		current_val = seed;
 		this.mult = mult;
 		this.increment = increment;
 		this.mod = mod;
 		this.java = use_java;
-		if(java)rand = new Random();
+		if (java)
+			rand = new Random();
 	}
-	
-	 /**
-	  * Generiert einen (pseudo-)zufälligen Wert zwischen 0 und mod
-	  * @return Zufallszahl
-	  */
-	public int random()
-	{
-		if(java)
-		{
+
+	/**
+	 * Generiert einen (pseudo-)zufaelligen Wert zwischen 0 und mod
+	 * 
+	 * @return Zufallszahl
+	 */
+	public int random() {
+		if (java) {
 			current_val = rand.nextInt(mod);
-		}
-		else
-			current_val = (mult *current_val + increment) % mod;
+		} else
+			current_val = (mult * current_val + increment) % mod;
 		return current_val;
 	}
+
 	/**
-	  * Generiert eine Liste von (pseudo-)zufälligen Werten zwischen 0 und mod
-	  * @return Zufallszahl-Array
-	  */
-	public int[] randomArray(int size)
-	{
+	 * Generiert eine Liste von (pseudo-)zufaelligen Werten zwischen 0 und mod
+	 * 
+	 * @return Zufallszahl-Array
+	 */
+	public int[] randomArray(int size) {
 		int[] ret = new int[size];
-		for(int i = 0;  i<size ; i++)ret[i]=random();
+		for (int i = 0; i < size; i++)
+			ret[i] = random();
 		return ret;
 	}
 }
